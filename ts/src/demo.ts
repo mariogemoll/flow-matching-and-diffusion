9851214ad94d7880b2b24a13b905fc701818fe03
Mario Gemoll <mariogemoll@gmx.net>
2025-09-30 11:27:22 +0200

    Add ESLint

--- ts/src/demo.ts
+++ ts/src/demo.ts
@@ -1,19 +1,19 @@
+import { addFrameUsingScales, defaultMargins, getContext  } from './web-ui-common/canvas';
+import { el } from './web-ui-common/dom';
 import { makeScale } from './web-ui-common/util';
-import { el } from "./web-ui-common/dom";
-import { getContext, defaultMargins, addFrameUsingScales, } from "./web-ui-common/canvas";
 
-function setUpFrameExample() {
-  const canvas = el(document, "#frame-canvas") as HTMLCanvasElement;
+function setUpFrameExample(): void {
+  const canvas = el(document, '#frame-canvas') as HTMLCanvasElement;
   const ctx = getContext(canvas);
   const xRange = [0, 100] as [number, number];
   const yRange = [0, 360] as [number, number];
-  const margins = defaultMargins
+  const margins = defaultMargins;
   const xScale = makeScale(xRange, [margins.left, canvas.width - margins.right]);
   const yScale = makeScale(yRange, [canvas.height - margins.bottom, margins.top]);
   addFrameUsingScales(ctx, xScale, yScale, 10);
 }
 
-async function run() {
+function run(): void {
   setUpFrameExample();
 }
 
