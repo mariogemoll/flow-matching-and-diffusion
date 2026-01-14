4f84090e31672de6c066090d5f109e1a7789dd8f
Mario Gemoll <mariogemoll@mailbox.org>
2026-01-14 23:49:57 +0100

    Change default domain/viewport

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const CANVAS_WIDTH = 400;
 export const CANVAS_HEIGHT = 300;
 
-export const X_DOMAIN: [number, number] = [-2, 2];
-export const Y_DOMAIN: [number, number] = [-1.5, 1.5];
+export const X_DOMAIN: [number, number] = [-4, 4];
+export const Y_DOMAIN: [number, number] = [-3, 3];
 
 export const NUM_TRAJECTORY_STEPS = 100;
